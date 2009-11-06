34f33329f478c9a859fb9fbe791fa1fbef98e258
Tommy van der Vorst <tommy@pixelspark.nl>
2009-11-06 11:01:31 +0000

    TJFabric: - Added SConstruct script to build .deb-package; also added the necessary scripts and program functions to run as daemon. The TJ Fabric Server (package name 'tjfabricserver') installs to /usr/share/tjfabricserver, is run from /etc/init.d/tjfabricd, saves its PID in /var/run/tjfabricd.pid and reads the configuration files from /etc/fabric.d/*.fabric. It requires mdnsd to be running, although this is not packaged (in fact there is no Debian package); people will have to build this themselves.
    All:
    - Added SConstruct files for the Linux armel build!
    
    
    
    git-svn-id: https://svn.pixelspark.nl/svn/doip/trunk@607 984cb85d-a4ae-4555-b754-76bc93324474

--- EP/TJFabric/src/tjfabricmain.cpp
+++ EP/TJFabric/src/tjfabricmain.cpp
@@ -10,53 +10,126 @@
 #include <iomanip>
 #include <sstream>
 #include <signal.h>
+
+#ifdef TJ_OS_POSIX
+	#include <sys/types.h>
+	#include <unistd.h>
+	#include <stdio.h>
+	#include <fcntl.h>
+#endif
+
 using namespace tj::shared;
 using namespace tj::fabric;
 using namespace tj::script;
 using namespace tj::np;
 using namespace tj::ep;
 
-Event _globalStop;
+Event _signalOccurred;
+int _lastSignal = 0;
 
 void sigIntHandler(int s) {
-	if(s==SIGINT) {
-		_globalStop.Signal();
-	}
+	_lastSignal = s;
+	_signalOccurred.Signal();
 }
 
 int main(int argc, char** argv) {
 	signal(SIGINT, sigIntHandler);
+	signal(SIGHUP, sigIntHandler);
+	signal(SIGCHLD, sigIntHandler);
+	
+	#ifdef TJ_OS_MAC
+		signal(SIGINFO, sigIntHandler);
+	#endif
 	
 	try {
 		Log::SetLogToConsole(true);
 		Log::Write(L"TJFabric/Main", std::wstring(L"Starting at t=")+Timestamp(true).ToString());
 		
+		bool runningAsDaemon = false;
+		if(argc>1) {
+			std::string parameter = argv[1];
+			if(parameter=="--daemon") {
+				runningAsDaemon = true;
+				#ifdef TJ_OS_POSIX
+					Log::Write(L"TJFabric/Main", L"Will run as daemon");
+					// Fork off a child process that continues to run, while the parent exits
+					int i = fork();
+					if(i<0) {
+						exit(1); /* fork error */
+					}
+					else if(i>0) {
+						exit(0); /* parent exits */
+					}
+					
+					// Make process independent
+					setsid();
+					chdir("/tmp");
+					
+					// Restrict file creation (mode will be 750)
+					umask(027);
+					
+					// Let's see if we're the only ones running
+					int lfp = open("/var/run/tjfabricd.pid", O_RDWR|O_CREAT, 0640);
+					if (lfp<0) {
+						Log::Write(L"TJFabric/Main", L"Cannot open lock file");
+						exit(1); /* can not open */
+					}
+					
+					if(lockf(lfp,F_TLOCK,0)<0) {
+						Log::Write(L"TJFabric/Main", L"Cannot lock file; is another instance already running?");
+						exit(0); /* can not lock */
+					}
+					
+					/* save PID to lock file */
+					std::ostringstream wos;
+					wos << getpid() << '\n';
+					std::string pidString = wos.str();
+					write(lfp, pidString.c_str(),pidString.length());
+				#endif
+				
+			}
+		}
+		
+		// Load fabric configuration files
 		std::map<std::string, ref<FabricEngine> > fabrics;
-		if(argc<2) {
-			Log::Write(L"TJFabric/Main", L"No fabric configuration file given, using ./default.fabric");
+		for(int a=(runningAsDaemon ? 2 : 1); a < argc; a++) {
 			ref<FabricEngine> engine = GC::Hold(new FabricEngine());
 			strong<Fabric> fabric = engine->GetFabric();
-			Fabric::LoadRecursive("./default.fabric", fabric);
+			Fabric::LoadRecursive(argv[a], fabric);
 			engine->Connect(true);
-			fabrics[""] = engine;
-		}
-		else {
-			for(int a=1; a < argc; a++) {
-				ref<FabricEngine> engine = GC::Hold(new FabricEngine());
-				strong<Fabric> fabric = engine->GetFabric();
-				Fabric::LoadRecursive(argv[a], fabric);
-				engine->Connect(true);
-				fabrics[argv[a]] = engine;
+			fabrics[argv[a]] = engine;
 
-				if(a==(argc-1)) {
-					// Last fabric, send init message
-					engine->GetQueue()->Add(GC::Hold(new Message(L"init")), null, null);
-				}
+			if(a==(argc-1)) {
+				// Last fabric, send init message
+				engine->GetQueue()->Add(GC::Hold(new Message(L"init")), null, null);
 			}
 		}
 		
 		// Wait for stop (interruption with SIGINT will signal _globalStop)
-		_globalStop.Wait();
+		while(true) {
+			_signalOccurred.Wait();
+			if(_lastSignal==SIGINT) {
+				break;
+			}
+			#ifdef TJ_OS_MAC
+				else if(_lastSignal==SIGINFO) {
+					std::wostringstream info;
+					info << L"GC: " << tj::shared::intern::Resource::GetResourceCount() << L"Threads: " << Thread::GetThreadCount();
+					Log::Write(L"TJFabric/Main", info.str());
+				}
+			#endif
+			else if(_lastSignal==SIGHUP) {
+				// Reload config?
+				Log::Write(L"TJFabric/Main", L"Received hang-up signal; will reload configuration");
+			}
+			else if(_lastSignal==SIGCHLD) {
+				Log::Write(L"TJFabric/Main", L"A child process terminated");
+			}
+			else {
+				break;
+			}
+		}
+		
 		fabrics.clear();
 	}
 	catch(const ScriptException& e) {
