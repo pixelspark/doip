2a07a72c7bece2e96449102457809a228bf62e9b
Tommy van der Vorst <tommy@pixelspark.nl>
2010-03-20 21:58:30 +0000

    EPFramework: - Forgot to commit ependpointdefinition.h/.cpp, which has been split-off from ependpoint.h/.cpp and only contains the classes related to the actual definitions (i.e. not the interfaces) in EP.
    git-svn-id: https://svn.pixelspark.nl/svn/doip/trunk@747 984cb85d-a4ae-4555-b754-76bc93324474

---
+++ EP/EP/include/ependpointdefinition.h
@@ -0,0 +1,144 @@
+#ifndef _TJ_EP_EPENDPOINT_DEFINITION_H
+#define _TJ_EP_EPENDPOINT_DEFINITION_H
+
+#include "epinternal.h"
+#include "epmessage.h"
+#include "ependpoint.h"
+
+#pragma warning (push)
+#pragma warning (disable: 4251 4275)
+
+namespace tj {
+	namespace ep {
+		class EP_EXPORTED EPEndpointDefinition: public EPEndpoint, public tj::shared::Serializable {
+			public:
+				EPEndpointDefinition();
+				virtual ~EPEndpointDefinition();
+				virtual void Clone();
+				
+				virtual tj::shared::String GetID() const;
+				virtual tj::shared::String GetNamespace() const;
+				virtual tj::shared::String GetFullIdentifier() const;
+				virtual tj::shared::String GetFriendlyName() const;
+				virtual tj::shared::String GetVersion() const;
+				virtual bool IsDynamic() const;
+				virtual EPMediationLevel GetMediationLevel() const;
+				virtual void GetMethods(std::vector< tj::shared::ref<EPMethod> >& methodList) const;
+				virtual void GetTransports(std::vector< tj::shared::ref<EPTransport> >& transportsList) const;
+				virtual void GetTags(std::set<EPTag>& tagList) const;
+				
+				virtual void Load(TiXmlElement* me);
+				virtual void Save(TiXmlElement* me);
+				
+			protected:
+				tj::shared::String _id;
+				tj::shared::String _namespace;
+				tj::shared::String _friendlyName;
+				tj::shared::String _version;
+				EPMediationLevel _level;
+				bool _dynamic;
+				std::vector< tj::shared::ref<EPMethod> > _methods;
+				std::vector< tj::shared::ref<EPTransport> > _transports;
+				std::set<EPTag> _tags;
+		};
+
+		class EP_EXPORTED EPMethodDefinition: public EPMethod, public tj::shared::Serializable {
+			public:
+				EPMethodDefinition();
+				EPMethodDefinition(const tj::shared::String& ids, const tj::shared::String& firstPath, const tj::shared::String& friendlyName);
+				virtual ~EPMethodDefinition();
+				virtual tj::shared::String GetID() const;
+				virtual tj::shared::String GetFriendlyName() const;
+				virtual tj::shared::String GetDescription() const;
+				virtual void  GetPaths(std::set<EPPath>& pathList) const;
+				virtual void GetParameters(std::vector< tj::shared::ref<EPParameter> >& parameterList) const;
+				virtual void Load(TiXmlElement* me);
+				virtual void Save(TiXmlElement* me);
+				virtual void Clone();
+				virtual tj::shared::String GetEnabledBinding() const;
+				
+				virtual void SetID(const tj::shared::String& i);
+				virtual void SetFriendlyName(const tj::shared::String& fn);
+				virtual void SetDescription(const tj::shared::String& ds);
+				virtual void AddPath(const EPPath& pt);
+				virtual void AddParameter(tj::shared::ref<EPParameter> p);
+				
+			protected:
+				tj::shared::String _id;
+				tj::shared::String _friendlyName;
+				std::set<EPPath> _paths;
+				tj::shared::String _description;
+				tj::shared::String _enabledBinding;
+				std::vector< tj::shared::ref<EPParameter> > _parameters;
+		};
+
+		class EP_EXPORTED EPReplyDefinition: public EPReply, public tj::shared::Serializable {
+			public:
+				EPReplyDefinition();
+				virtual ~EPReplyDefinition();
+				virtual tj::shared::String GetPath() const;
+				virtual void GetParameters(std::vector< tj::shared::ref<EPParameter> >& parameterList) const;
+				virtual void Load(TiXmlElement* me);
+				virtual void Save(TiXmlElement* me);
+				
+			protected:
+				tj::shared::String _path;
+				std::vector< tj::shared::ref<EPParameter> > _parameters;
+		};
+
+		class EP_EXPORTED EPParameterDefinition: public EPParameter, public tj::shared::Serializable {
+			public:
+				EPParameterDefinition();
+				EPParameterDefinition(const tj::shared::String& friendlyName, const tj::shared::String& type, const tj::shared::String& minValue, const tj::shared::String& maxValue, const tj::shared::String& defaultValue, EPParameter::Nature = EPParameter::NatureUnknown, const tj::shared::String& valueBinding=L"");
+				virtual ~EPParameterDefinition();
+				virtual tj::shared::String GetFriendlyName() const;
+				virtual tj::shared::String GetType() const;
+				virtual tj::shared::Any::Type GetValueType() const;
+				virtual tj::shared::Any GetMinimumValue() const;
+				virtual tj::shared::Any GetMaximumValue() const;
+				virtual tj::shared::Any GetDefaultValue() const;;
+				virtual void SetDefaultValue(const tj::shared::Any& val);
+				virtual tj::shared::String GetValueBinding() const;
+				virtual void SetValueBinding(const tj::shared::String& i);
+				virtual EPParameter::Nature GetNature() const;
+				virtual void Load(TiXmlElement* me);
+				virtual void Save(TiXmlElement* me);
+				virtual bool HasOptions() const;
+				virtual void GetOptions(std::set< EPOption >& optionList) const;
+				virtual void AddOption(const EPOption& epo);
+				
+			protected:
+				tj::shared::String _friendlyName;
+				tj::shared::String _type;
+				tj::shared::String _minimumValue;
+				tj::shared::String _maximumValue;
+				tj::shared::String _defaultValue;
+				tj::shared::String _valueBinding;
+				tj::shared::Any _runtimeDefaultValue;
+				std::set< EPOption > _options;
+				EPParameter::Nature _nature;
+		};
+
+		class EP_EXPORTED EPTransportDefinition: public EPTransport, public tj::shared::Serializable {
+			public:
+				EPTransportDefinition();
+				~EPTransportDefinition();
+				virtual tj::shared::String GetType() const;
+				virtual tj::shared::String GetAddress() const;
+				virtual tj::shared::String GetFormat() const;
+				virtual tj::shared::String GetFraming() const;
+				virtual unsigned short GetPort() const;
+				virtual void Save(TiXmlElement* me);
+				virtual void Load(TiXmlElement* me);
+				
+			protected:
+				tj::shared::String _type;
+				tj::shared::String _address;
+				tj::shared::String _format;
+				tj::shared::String _framing;
+				unsigned short _port;
+		};
+	}
+}
+
+#endif
